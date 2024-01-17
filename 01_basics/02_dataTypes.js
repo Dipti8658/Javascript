e49520f24a301ebf8257a032b9b0363d014cf86e
Dipti8658 <138275129+Dipti8658@users.noreply.github.com>
2024-01-17 13:15:19 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3 ); // we are using nodejs , not browser so it gives error
+
+console.log(3 + 
+    3) // code readability should be high
+
+console.log("Dipti");
+
+let name = "dipti"
+let age = 18
+let isLoggedIn = false
+let state; // undefined
+
+// number =>2 to power 53
+// bigint
+// string =>""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof "dipti");
+console.log(typeof age);
+console.log(typeof null); // o/p ->object
+console.log(typeof undefined); // o/p ->undefined
