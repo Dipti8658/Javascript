1c7ea96b8365238bf04a3ffdd2bf43cd21a40a33
Dipti8658 <138275129+Dipti8658@users.noreply.github.com>
2024-01-17 14:10:24 +0000

    datatypesummary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,54 @@
+
+// Primitive
+
+// It is of 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const Id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(Id === anotherId);
+
+// const bigNumber = 45566677777777543456453n
+
+
+// Non-primitive(Reference type)
+
+// Array, Objects , Functions
+
+const heros = ["shaktiman", "naagraj", "doga"]; // arrays
+
+//object
+let myObj = {
+    name: "Dipti",
+    age: 22,
+}
+
+//function
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+// How to know datatype of a variable
+console.log(typeof myFunction);
+
+// Typeof Vale : Result
+//-------------------------
+// Undefined -> "undefined"
+// Null ->	"object"
+// Boolean -> "boolean"
+// Number -> "number"
+// String -> "string"
+// Object (native and does not implement [[Call]]) ->	"object"
+// Object (native or host and does implement [[Call]]) ->	"function"
+// Object (host and does not implement [[Call]]) ->	Implementation-defined except may not be "undefined", "boolean", "number", or "string".
+
+// function -> object-function
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
